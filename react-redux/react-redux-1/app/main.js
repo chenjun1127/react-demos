86af3cc8c70d8ded43ff81ffa0594b93f82e0b8c
chenjun <kiss19861127@126.com>
2018-05-31 16:49:49 +0800

    delete

--- react-redux/react-redux-1/app/main.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppComponent from '../components/MainComponent';
-ReactDOM.render(<AppComponent/>, document.getElementById('content'));
--- react-redux/react-redux-2/actions/index.js
+++
@@ -1,9 +0,0 @@
-const increaseAction = (count, text) => {
-    return {type: 'increase', count, text}
-}
-
-const decreaseAction = (count, text) => {
-    return {type: 'decrease', count, text}
-}
-
-export {increaseAction, decreaseAction}
